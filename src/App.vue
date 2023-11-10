354db5aba484badfe6ef958c7dd39d366bfe4a6e
HCK42MRX <133762067+HCK42MRX@users.noreply.github.com>
2023-11-10 14:36:30 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+  <div id="App">
+    <header v-if="$route.meta.header === 1">
+      <navMenu></navMenu>
+    </header>
+    <router-view></router-view>
+    <footer v-if="$route.meta.footer === 1">
+      <footerMenu></footerMenu>
+    </footer>
+  </div>
+</template>
+<script>
+import navMenu from "./components/navigation.vue"
+import footerMenu from "./components/footer.vue"
+
+export default {
+  name : "App",
+  components :{
+    navMenu,
+    footerMenu,
+  }
+  
+}
+</script>
+<style lang="css">
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+
+html,body {
+  background-color: #060606;
+  font-family: 'Poppins',sans-serif;
+}
+  
+</style>
